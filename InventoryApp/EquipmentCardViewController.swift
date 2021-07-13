167061585565b74555128e7a70ecfb20f57e335d
Yuri Martinovsky <vvrite@me.com>
2021-07-13 17:21:39 +0300

    Added EquipmentCardViewController

---
+++ InventoryApp/EquipmentCardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EquipmentCardViewController.swift
+//  InventoryApp
+//
+//  Created by Yuriy Martinovskiy on 13.07.2021.
+//
+
+import UIKit
+
+class EquipmentCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
