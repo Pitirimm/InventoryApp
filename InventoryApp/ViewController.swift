07976818d023963a84cfddfbbf829ab9e7833138
Yuri Martinovsky <vvrite@me.com>
2021-07-13 14:23:05 +0300

    Initial Commit

---
+++ InventoryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InventoryApp
+//
+//  Created by Yuriy Martinovskiy on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
