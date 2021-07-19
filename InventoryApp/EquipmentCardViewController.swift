1ecfce96184bb02d138932dc2d170d2e6d68c8b5
Yuri Martinovsky <vvrite@me.com>
2021-07-19 22:28:26 +0300

    Added Edit button and Delete button

--- InventoryApp/EquipmentCardViewController.swift
+++ InventoryApp/EquipmentCardViewController.swift
@@ -6,11 +6,43 @@
 //
 
 import UIKit
+import CoreData
 
 class EquipmentCardViewController: UIViewController {
-
+    
+    var equipments: [Equipment] = []
+    
+    @IBOutlet weak var inventoryNumber: UITextField!
+    @IBOutlet weak var typeEquipment: UITextField!
+    @IBOutlet weak var modelEquipment: UITextField!
+    @IBOutlet weak var serialNumberEquipment: UITextField!
+    @IBOutlet weak var dateOfCreation: UITextField!
+    
+    @IBOutlet weak var surnamePerson: UITextField!
+    @IBOutlet weak var namePerson: UITextField!
+    @IBOutlet weak var departmentPerson: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func pressSaveButton(_ sender: UIBarButtonItem) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Equipment", in: context) else { return }
+        
+        let equipmentObject = Equipment(entity: entity, insertInto: context)
+        equipmentObject.type = typeEquipment.text
+        equipmentObject.model = modelEquipment.text
+        equipmentObject.inventoryNumber = inventoryNumber.text
+        equipmentObject.serialNumber = serialNumberEquipment.text
+        
+        do {
+            try context.save()
+            equipments.append(equipmentObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
 }
--- InventoryApp/InventoryTableViewController.swift
+++ InventoryApp/InventoryTableViewController.swift
@@ -6,13 +6,37 @@
 //
 
 import UIKit
+import CoreData
 
 class InventoryTableViewController: UITableViewController {
+    
+    var equipments: [Equipment] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 75
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Equipment> = Equipment.fetchRequest()
+        
+        do {
+            equipments = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func addEquipment(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "toEquipmentCard", sender: nil)
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -22,19 +46,54 @@ class InventoryTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return equipments.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        cell.modelEquipment.text = "INTEL DH77EB"
-        cell.serialNumberEquipment.text = "S/N: GEBN6510066934"
+        cell.modelEquipment.text = equipments[indexPath.row].model
+        cell.serialNumberEquipment.text = equipments[indexPath.row].serialNumber
         cell.ownerEquipment.text = "Ivanov Andrey Fedorovich"
-        cell.inventoryNumberEquipment.text = "101345"
+        cell.inventoryNumberEquipment.text = equipments[indexPath.row].inventoryNumber
         cell.logoEquipment.image = UIImage(systemName: "desktopcomputer")
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let currentList = equipments[indexPath.row]
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let deleteAction = UIContextualAction(style: .destructive,
+                                              title: "Delete") { _, _, _  in
+            
+            context.delete(currentList)
+            do {
+                try context.save()
+                self.equipments.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        
+        let editAction = UIContextualAction(style: .normal,
+                                              title: "Edit") { _, _, _  in
+            
+            context.delete(currentList)
+            do {
+                try context.save()
+                self.equipments.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        editAction.backgroundColor = .green
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction, editAction])
     }
 }
